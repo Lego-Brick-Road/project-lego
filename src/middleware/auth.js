ccbe1cb93d989d50b9ee482f479e40404ec58e71
Benjamin Clark <benisin@gmail.com>
2019-09-09 10:22:08 -0700

    fixed lint problems

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -8,12 +8,12 @@ module.exports = capability => {
       let [authType, authString] = req.headers.authorization.split(/\s+/);
 
       switch (authType.toLowerCase()) {
-        case 'basic':
-          return _authBasic(authString);
-        case 'bearer':
-          return _authBearer(authString);
-        default:
-          return _authError();
+      case 'basic':
+        return _authBasic(authString);
+      case 'bearer':
+        return _authBearer(authString);
+      default:
+        return _authError();
       }
     } catch (e) {
       _authError();
