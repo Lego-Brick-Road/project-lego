5435db0c993668abf064beffaf6a804288cfaa71
joannaarroyo <joliuac@gmail.com>
2019-09-10 15:12:13 -0700

    removed redirect from router.js

--- src/router.js
+++ src/router.js
@@ -16,7 +16,6 @@ apiRouter.post('/signup', (req, res, next) => {
       req.user = user;
       res.set('token', req.token);
       res.cookie('auth', req.token);
-      res.redirect('/train');
       res.send(req.token);
     })
     .catch(next);
